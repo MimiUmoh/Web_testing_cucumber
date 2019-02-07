93e86e05e90fb15d38802efcfaf4874259651f4a
Tech-A35x <tech-a35x@Tech-A35s-MacBook-Air.local>
2019-02-07 14:17:50 +0000

    initial push, set up, features and scenario created and looking to create a POM around the bbc website

---
+++ features/step_definitions/bbc_login_step_defs.rb
@@ -0,0 +1,22 @@
+#copy and paste the code block for you, we are going ot use our POM ro run mehtods in here tointeract with the page. we will also use assertion libaries to check values.  
+
+
+Given("I access the bbc login page") do
+  visit('http://www.bbc.co.uk')
+end
+
+Given("I input incorrect username details") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("I input incorrect password details") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I try to log in") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I recieve an arror for not finding the account") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
